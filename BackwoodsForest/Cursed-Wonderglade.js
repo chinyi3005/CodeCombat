3617e6449715e21a84ebb98183d1811c464f7744
Chinyi <chinyi3005@gmail.com>
2016-08-22 18:59:45 +0800

    complete 2 levels

---
+++ BackwoodsForest/Cursed-Wonderglade.js
@@ -0,0 +1,22 @@
+// Level: Cursed Wonderglade
+// https://codecombat.com/play/level/cursed-wonderglade
+
+// Wonderglade has changed since our last visit.
+// Ogres cursed it and we should defeat them.
+// The burl still is collecting gems, so don't touch them.
+// Also don't attack the burl.
+
+while (true) {
+    // Find the nearest item.
+    // Collect it (if it exists) only if its type isn't "gem".
+    var item = hero.findNearestItem();
+    if (item && item.type != "gem") {
+        hero.move(item.pos);
+    }
+    // Find the nearest enemy.
+    // Attack it if it exists and its type isn't "burl".
+    var enemy = hero.findNearestEnemy();
+    if (enemy && enemy.type != "burl") {
+        hero.attack(enemy);
+    }
+}
