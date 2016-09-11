94ed30e7c509483e6c111e809bd59b79e2ff8a9f
Chinyi <chinyi3005@gmail.com>
2016-09-11 18:58:40 +0800

    add level: The Great Yak Stampede

---
+++ SarvenDesert/The-Great-Yak-Stampede.js
@@ -0,0 +1,13 @@
+// Level: The Great Yak Stampede
+// https://codecombat.com/play/level/the-great-yak-stampede
+
+// Save the village by blocking the path of the incoming sand yak herd.
+// Move closer to the red X.
+// Build a fence at the red X.
+
+while(true) {
+    hero.moveXY(54, 17);
+    hero.moveXY(12, 40);
+    hero.buildXY("fence", 38, 53);
+    break;
+}
